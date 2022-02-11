92eb7fe15a0cb3506c35bf1c55061bdbf1b88086
rafabnu <99256219+rafabnu@users.noreply.github.com>
2022-02-11 16:01:34 -0300

    Create Carabiner.cpp

---
+++ Carabiner.cpp
@@ -0,0 +1,389 @@
+class CfgPatches
+{
+	class Carabiner
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data",
+			"DZ_Characters",
+			"DZ_Characters_Belts",
+			"DZ_Weapons_Melee"
+		};
+	};
+};
+class CfgMods
+{
+	class Carabiner
+	{
+		dir="Carabiner";
+		picture=""; 
+		action="";
+		hideName=1;
+		hidePicture=1;
+		name="Carabiner";
+		credits=""; 
+		author="";
+		authorID="0";  
+		version="1.0";
+		extra=0;
+		type="mod";
+		dependencies[]=
+		{
+			"Game",
+			"World",
+			"Mission"
+		};
+		class defs
+		{
+			class gameScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"Carabiner/scripts/3_game"
+				};
+			};
+			class worldScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"Carabiner/scripts/4_world"
+				};
+			};
+			class missionScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"Carabiner/scripts/5_mission"
+				};
+			};
+		};
+	};
+};
+class cfgSlots
+{
+	class Slot_IceTool
+	{
+		name="IceTool";
+		displayName="IceTool";
+		ghostIcon="";
+	};
+};
+class CfgNonAIVehicles
+{
+	class ProxyAttachment;
+	class ProxyGloves: ProxyAttachment
+	{
+		scope=2;
+		inventorySlot[]=
+		{
+			"Gloves"
+		};		
+		model="\Carabiner\proxy\Gloves.p3d";
+	};
+	class ProxyAttachment;
+	class ProxyIceTool: ProxyAttachment
+	{
+		scope=2;
+		inventorySlot[]=
+		{
+			"IceTool"
+		};		
+		model="\Carabiner\proxy\IceTool_slot.p3d";
+	};
+};
+class CfgVehicles
+{
+	class Clothing;
+	class Carabiner: Clothing
+	{
+		scope=2;
+		displayName="Carabiner";
+		descriptionShort="Carabiner by Rafa";
+		model="\Carabiner\Carabiner.p3d";			
+		inventorySlot[]=
+		{
+			"Belt_Back"
+		};
+		vehicleClass="Clothing";
+		itemInfo[]=
+		{
+			"Clothing",
+			"Belt"
+		};
+		weight=110;
+		itemSize[]={1,1};
+		attachments[]=
+		{
+			"gloves",
+			"IceTool"
+		};
+		absorbency=0.1;
+		heatIsolation=0;
+		repairableWithKits[]={3};
+		repairCosts[]={25};
+		hiddenSelections[]=
+		{
+			"Carabiner"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\Carabiner\data\Carabiner_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"Carabiner\data\Carabiner.rvmat"
+							}
+						};
+						
+						{
+							0.69999999,
+							
+							{
+								"Carabiner\data\Carabiner.rvmat"
+							}
+						};
+						
+						{
+							0.5,
+							
+							{
+								"Carabiner\data\Carabiner_damage.rvmat"
+							}
+						};
+						
+						{
+							0.30000001,
+							
+							{
+								"Carabiner\data\Carabiner_damage.rvmat"
+							}
+						};
+						
+						{
+							0,
+							
+							{
+								"Carabiner\data\Carabiner_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Inventory_Base;
+		class IceTool: Inventory_Base
+		{
+			inventorySlot[]=
+			{
+				"IceTool"
+			};		
+		};
+		class Gloves: Inventory_Base
+		{
+			inventorySlot[]=
+			{
+				"Gloves"
+			};		
+		};				
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickUpItem
+				{
+					soundSet="WorkingGloves_pickup_SoundSet";
+					id=797;
+				};
+				class drop
+				{
+					soundset="WorkingGloves_drop_SoundSet";
+					id=898;
+				};
+			};
+		};
+	};
+	/*class Inventory_Base;
+	class Climbing: Inventory_Base
+	{
+		scope=2;
+		displayName="Ice Climbing Axe";
+		descriptionShort="Ice Climbing Axe create by Rafa";	
+		model="\Carabiner\IceTool.p3d";
+		build_action_type=10;
+		dismantle_action_type=74;
+		repairableWithKits[]={4};
+		repairCosts[]={18};
+		rotationFlags=17;
+		weight=1133;
+		itemSize[]={2,4};
+		fragility=0.0099999998;
+		inventorySlot[]=
+		{
+			"Climbing"			
+		};
+		lootCategory="Tools";		
+		lootTag[]=
+		{
+			"Work",
+			"Forester",
+			"Camping"
+		};
+		itemInfo[]=
+		{
+			"Axe"
+		};
+		openItemSpillRange[]={20,40};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=150;
+					healthLevels[]=
+					{
+						
+						{
+							1.00,
+							
+							{
+								"Carabiner\data\IceTool.rvmat"
+							}
+						};
+						
+						{
+							0.69999999,
+							
+							{
+								"Carabiner\data\IceTool.rvmat"
+							}
+						};
+						
+						{
+							0.51,
+							
+							{
+								"Carabiner\data\IceTool_damage.rvmat"
+							}
+						};
+						
+						{
+							0.30000001,
+							
+							{
+								"Carabiner\data\IceTool_damage.rvmat"
+							}
+						};
+						
+						{
+							0.01,
+							
+							{
+								"Carabiner\data\IceTool_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class MeleeModes
+		{
+			class Default
+			{
+				ammo="MeleeHatchet";
+				range=1.4;
+			};
+			class Heavy
+			{
+				ammo="MeleeHatchet_Heavy";
+				range=1.4;
+			};
+			class Sprint
+			{
+				ammo="MeleeHatchet_Heavy";
+				range=3.3;
+			};
+		};
+		isMeleeWeapon=1;
+		suicideAnim="woodaxe";
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup_light
+				{
+					soundSet="hatchet_pickup_light_SoundSet";
+					id=796;
+				};
+				class pickup
+				{
+					soundSet="hatchet_pickup_SoundSet";
+					id=797;
+				};
+				class drop
+				{
+					soundset="woodaxe_drop_SoundSet";
+					id=898;
+				};
+				class FirefighterAxe_loop_SoundSet
+				{
+					soundSet="FirefighterAxe_loop_SoundSet";
+					id=1121;
+				};
+				class FirefighterAxe_end_SoundSet
+				{
+					soundSet="FirefighterAxe_end_SoundSet";
+					id=1122;
+				};
+				class ShoulderR_Hide
+				{
+					soundset="ShoulderR_Hide_SoundSet";
+					id=1210;
+				};
+				class ShoulderR_Show
+				{
+					soundset="ShoulderR_Show_SoundSet";
+					id=1211;
+				};
+				class WoodHammer_SoundSet
+				{
+					soundSet="WoodHammer_SoundSet";
+					id=11161;
+				};
+				class animalSkinning_in
+				{
+					soundSet="animalSkinning_in_SoundSet";
+					id=516;
+				};
+				class animalSkinning
+				{
+					soundSet="animalSkinning_SoundSet";
+					id=517;
+				};
+				class animalSkinning_out
+				{
+					soundSet="animalSkinning_out_SoundSet";
+					id=518;
+				};
+			};
+		};
+	};*/ 
+};
